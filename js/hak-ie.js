9711c8ae2f847ba3f5fd178cb3f3de2eb7820d24
Roman Tashev <rzet777@gmail.com>
2019-01-24 15:25:36 +0300

    hak for ie

---
+++ js/hak-ie.js
@@ -0,0 +1,2 @@
+if (/MSIE 10/i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) || /Edge\/\d./i.test(navigator.userAgent)) 
+{document.write('<style>.dignity{background-color:#5165e2;}</style>')}
