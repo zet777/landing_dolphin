f9e0d415f36c42f83a6bc214dc3045454b55e3c6
Roman Tashev <rzet777@gmail.com>
2019-01-28 13:02:55 +0300

    Edit

--- js/hak-ie.js
+++ js/hak-ie.js
@@ -1,4 +1,7 @@
-if (/MSIE 10/i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) || /Edge\/\d./i.test(navigator.userAgent)) 
+if (/MSIE 10/i.test(navigator.userAgent) || 
+	/MSIE 9/i.test(navigator.userAgent) || 
+	/rv:11.0/i.test(navigator.userAgent) || 
+	/Edge\/\d./i.test(navigator.userAgent)) 
 {document.write
 	('<style>.dignity{background-color:rgba(57, 172, 249, 1);}header{background-color: rgba(57, 172, 249, 1);}.header{background-color: rgba(57, 172, 249, 1);}.hak{background: rgba(57, 172, 249, 1);}.about{background-color: #fff;}</style>')
 }
