385007d8250244d7b67b3114122b2d357094e5fa
Roman Tashev <rzet777@gmail.com>
2019-03-20 13:49:18 +0300

    Update send.php

--- send.php
+++ send.php
@@ -5,7 +5,7 @@ if($_POST)
     $subject = "Заказ звонка"; //ТЕМА
     $message = '<span style="font-weight:bold;color:#ff6600;font-size:18px;"><i>Заказ звонка</i> </span><br><br>
     Имя: <span style="font-weight:bold;color:#339900;">'.$_POST['name'].'</span><br>
-    Телефон: <span style="font-weight:bold;color:#339900;"> '.$_POST['telephone'].'</span>';
+    Телефон: <span style="font-weight:bold;color:#339900;"> '.$_POST['phone'].'</span>';
     $headers = "Content-type: text/html; charset=UTF-8 \r\n";
     $headers .= "From: <dolphin136@yandex.ru>\r\n"; // ОТ КОГО, ПРИДУМАЙТЕ ЯЩИК ИЛИ УКАЖИТЕ ПОЧТУ СВОЕГО САЙТА.
     $result = mail($to, $subject, $message, $headers);
