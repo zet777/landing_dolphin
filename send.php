24b225aa4c25a55de8c872d6588eccdc46fa16f4
Roman Tashev <rzet777@gmail.com>
2019-03-20 11:04:01 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,17 +1,17 @@
 <?php
-if($_POST)
-    {
-    $to = "salon@clubprint.ru"; //КУДА ОТПРАВЛЯТЬ ПИСЬМО
-    $subject = "Заказ звонка"; //ТЕМА
-    $message = '<span style="font-weight:bold;color:#ff6600;font-size:18px;"><i>Заказ звонка</i> </span><br><br>
-    Имя: <span style="font-weight:bold;color:#339900;">'.$_POST['name'].'</span><br>
-    Телефон: <span style="font-weight:bold;color:#339900;"> '.$_POST['phone'].'</span>';
-    $headers = "Content-type: text/html; charset=UTF-8 \r\n";
-    $headers .= "From: <salon@clubprint.ru>\r\n"; // ОТ КОГО, ПРИДУМАЙТЕ ЯЩИК ИЛИ УКАЖИТЕ ПОЧТУ СВОЕГО САЙТА.
-    $result = mail($to, $subject, $message, $headers);
-  
-    if ($result){
-        echo "<p>Сообщение успешно отправлено. Скоро Вам перезвонят</p>";
-    }
-    }
+ /* Проверка существования переменных */
+  if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+ if (isset($_POST['name'])) {$name = $_POST['name'];}
+
+ $address = "salon@clubprint.ru";
+
+/* А здесь прописывается текст сообщения, \n - перенос строки */
+ $mes = "Тема: Заказ обратного звонка!  \nТелефон: $phone\nИмя: $name";
+
+/* А эта функция как раз занимается отправкой письма на указанный вами email */
+$sub='Заказ'; //сабж
+$email='Заказ <clubprint.ru>'; // от кого
+ $send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+
+header("Location: /"); /* Для возврата на главную */
 ?>
