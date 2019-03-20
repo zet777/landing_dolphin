7760f624ac99d57cf20bc2394cb982cccd85e38a
Roman Tashev <rzet777@gmail.com>
2019-03-20 10:04:12 +0300

    Create send.php

---
+++ send.php
@@ -0,0 +1,17 @@
+<?php
+ /* Проверка существования переменных */
+  if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+ if (isset($_POST['name'])) {$name = $_POST['name'];}
+
+ $address = "salon@clubprint.ru";
+
+/* А здесь прописывается текст сообщения, \n - перенос строки */
+ $mes = "Тема: Заказ обратного звонка!  \nТелефон: $phone\nИмя: $name";
+
+/* А эта функция как раз занимается отправкой письма на указанный вами email */
+$sub='Заказ'; //сабж
+$email='Заказ <clubprint.ru>'; // от кого
+ $send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+
+
+?>
