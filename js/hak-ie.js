05b1c1ec6d37a86c42827e1637402a894c2ab85c
Roman Tashev <rzet777@gmail.com>
2019-03-28 12:04:26 +0300

    Update hak-ie.js

--- js/hak-ie.js
+++ js/hak-ie.js
@@ -3,7 +3,7 @@ if (/MSIE 10/i.test(navigator.userAgent) ||
 	/rv:11.0/i.test(navigator.userAgent) || 
 	/Edge\/\d./i.test(navigator.userAgent)) 
 {document.write
-	('<style>footer{background-color: rgba(57, 172, 249, 1);}.price{background:#ffffff;}.hakscr{background: #ffffff;}.dignity{background-color:rgba(57, 172, 249, 1);}header{background-color: rgba(57, 172, 249, 1);}.header{background-color: rgba(57, 172, 249, 1);}.hak{background: rgba(57, 172, 249, 1);}.about{background-color: #fff;}</style>')
+	('<style>.order{background-color:#fff;}footer{background-color: rgba(57, 172, 249, 1);}.price{background:#ffffff;}.hakscr{background: #ffffff;}.dignity{background-color:rgba(57, 172, 249, 1);}header{background-color: rgba(57, 172, 249, 1);}.header{background-color: rgba(57, 172, 249, 1);}.hak{background: rgba(57, 172, 249, 1);}.about{background-color: #fff;}</style>')
 }
 
 
