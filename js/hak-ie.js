f375c669748ee910a83c6cd17ae00208a583b502
Roman Tashev <rzet777@gmail.com>
2019-05-14 11:20:09 +0300

    Update hak-ie.js

--- js/hak-ie.js
+++ js/hak-ie.js
@@ -1,4 +1,4 @@
-/*if (/MSIE 10/i.test(navigator.userAgent) || 
+if (/MSIE 10/i.test(navigator.userAgent) || 
 	/MSIE 9/i.test(navigator.userAgent) || 
 	/rv:11.0/i.test(navigator.userAgent) || 
 	/Edge\/\d./i.test(navigator.userAgent)) 
@@ -8,4 +8,3 @@
 
 
 
-*/
